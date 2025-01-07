ee32cdfb0705e0d15d649dea9f089fd468cce392
Ajit raut <ajitraut9561@gmail.com>
2025-01-07 21:54:20 +0530

    User Edit card Changed

--- client/src/components/UserEditCard.jsx
+++ client/src/components/UserEditCard.jsx
@@ -27,20 +27,7 @@ const UserEditCard = ({ user }) => {
         <h2 className="card-title">{firstName + " " + lastName}</h2>
         {age && gender && <p>{age + ", " + gender}</p>}
         <p>{about}</p>
-        <div className="card-actions justify-center my-4">
-          <button
-            className="btn btn-primary"
-            onClick={() => handleSendRequest("ignored", _id)}
-          >
-            Ignore
-          </button>
-          <button
-            className="btn btn-secondary"
-            onClick={() => handleSendRequest("interested", _id)}
-          >
-            Interested
-          </button>
-        </div>
+       
       </div>
     </div>
   );
