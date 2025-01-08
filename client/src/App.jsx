cd632b9529e703c281d1e4ebdef31823e20c9f4f
Ajit raut <ajitraut9561@gmail.com>
2025-01-08 22:14:40 +0530

    Some changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import appStore from "./utils/appStore";
 import Feed from "./components/Feed";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -15,7 +16,8 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
-              <Route path="/" element={<Feed />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/feed" element={<Feed />} />
               <Route path="/login" element={<Login />} />
               <Route path="/profile" element={<Profile />} />
               <Route path="/connections" element={<Connections />} />
